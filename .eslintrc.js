6179e6c198707866e7901da50b282b665579ec50
Guy Sherman <g.sherman@crimsoneducation.org>
2021-06-05 17:44:38 +1200

    Basic setup typescript, eslint, jest, yarn
    We need testing, linting, etc.
    
    We're going to use ts-node because it is simple.

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+  env: {
+    node: true,
+    browser: true,
+    es2021: true
+  },
+  extends: [
+    'standard'
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module'
+  },
+  plugins: [
+    '@typescript-eslint',
+    'jest'
+  ],
+  rules: {
+  }
+}
