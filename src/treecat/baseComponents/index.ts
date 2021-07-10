76a88ce0f74d12255153c4510b712615b17a6174
Guy Sherman <guy@guysherman.com>
2021-07-10 14:00:08 +1200

    Cleaned up a messy console.log

--- src/treecat/baseComponents/index.ts
+++ src/treecat/baseComponents/index.ts
@@ -4,7 +4,6 @@ import { Fiber } from '../Fiber'
 export type NodeCreator<TNode> = (options: any | undefined) => TNode
 
 export function createNode<TNode, TNodeOptions> (element: Fiber, fnCreate: NodeCreator<TNode>): TNode {
-  console.log(`createNode: ${element.type}`)
   // eslint-disable-next-line no-unused-vars
   const { props: { children, ...options } } = element
   const node = fnCreate({ ...(options as TNodeOptions) })
