63fceab802b2b4abf33f3a1efe48a57034b32c9a
Guy Sherman <guy@guysherman.com>
2021-07-11 21:39:14 +1200

    Refined the tests for Fiber
    Specifically, checking that the dom object gets created,
    and that the siblings get returned if there are no children

--- src/treecat/Fiber.test.ts
+++ src/treecat/Fiber.test.ts
@@ -69,15 +69,28 @@ test('Box with multiple boxes', () => {
     }
   }
 
+  // We expect a newly returned unit of work to be without a dom
   const [workUnit] = performUnitOfWork(root)
+  expect(workUnit!.dom === undefined).toBe(true)
+
+  // We expect the navigation to children to work recursively
+  // and we expect the unit of work we just worked on to
+  // now have a dom
   const [child] = performUnitOfWork(workUnit as Fiber)
   expect(child!.type).toEqual('foo')
+  expect(workUnit!.dom === undefined).toBe(false)
 
+  // We expect the array of children to have been transformed
+  // into a linked list
   const { sibling } = child as Fiber
-  expect(sibling!.type).toEqual('bar')
-
   const { sibling: sibling2 } = sibling as Fiber
+  expect(sibling!.type).toEqual('bar')
   expect(sibling2!.type).toEqual('baz')
+
+  // When there are no more children, but there are siblings
+  // we expect performUnitOfWork to return the sibling
+  const [siblingRound2] = performUnitOfWork(child as Fiber)
+  expect(siblingRound2 === sibling).toBe(true)
 })
 
 afterEach(() => {
